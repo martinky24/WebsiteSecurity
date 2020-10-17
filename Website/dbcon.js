e1f7a0416c2aca166b9667b52cb9ecda60475cb7
nstapler <staplern@bu.edu>
2020-10-16 22:54:04 -0700

    Implement fake table init

--- Website/dbcon.js
+++ Website/dbcon.js
@@ -1,17 +1,25 @@
 /* Database setup */ 
 const { Client } = require('pg');
 
-const client = new Client({
-    user: 'coreuser',
-    host: 'team-web-security.cni5jxwbesmd.us-west-1.rds.amazonaws.com',
-    database: 'postgres',
-    password: 'corepass',
-    port: 5422,
-});
-
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res.rows)
-    client.end()
-});
+function getClient(){
+    return new Client({
+        user: 'coreuser',
+        password: 'corepass',
+        host: 'team-web-security.cni5jxwbesmd.us-west-1.rds.amazonaws.com',
+        database: 'banking',
+        port: 5422,
+    }); // can only connect and end connection once
+}
+function runQuery(query,callback){
+    var client = getClient()
+    client.connect()
+    client.query(query, (err, res) => {
+        client.end()
+        console.log(`Result of the query ${query}:\n`,"Errors:",err,"\nResults:",res)
+        if(callback){
+            callback(res)
+        }
+    });
+}
+exports.getDbClient = getClient;
+exports.runDBQuery = runQuery; // mainly just use this to take care of connection handling
