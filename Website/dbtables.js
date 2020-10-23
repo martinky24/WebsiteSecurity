3506847efc975e60a39d5462f956e28c7932f648
nstapler <staplern@bu.edu>
2020-10-22 22:33:48 -0700

    Refactor inserts

--- Website/dbtables.js
+++ Website/dbtables.js
@@ -10,34 +10,43 @@ function fillTables(rowCount){
         passLength = faker.random.number({'min':5,'max':10})
         fillUser("admin","admin")
         for (let i = 0; i < rowCount; i++) {
-            fillFullUser(faker.internet.password(passLength),faker.internet.userName())
+            fillFullUser(faker.internet.password(passLength), faker.internet.userName())
         }
     });
 }
+
 function fillUser(pass,username,callback){
     var hashed = "" //hashFunc(pass)
     var query = `INSERT INTO users VALUES (DEFAULT,'${pass}','${username}','${hashed}') RETURNING user_id`
     dbCon.runDBQuery(query,callback);
 }
+function fillPersonalInfo(userID,first,last,birth,email){
+    var query = `INSERT INTO personal_info VALUES (DEFAULT,'${first}','${last}','${birth}','${email}',${userID})`
+    dbCon.runDBQuery(query)
+}
+function fillFinancialInfo(userID,routeNum,accountNum,balance){
+    var query = `INSERT INTO financial_info VALUES (DEFAULT,${userID},${routeNum},${accountNum},${balance})`
+    dbCon.runDBQuery(query)
+}
+
 function fillFullUser(pass,username){
     fillUser(pass,username,(res)=>{
         console.log(res.rows[0].user_id);
         if(res && res.rows && res.rows.length > 0 && res.rows[0].user_id){
             userID = res.rows[0].user_id;
-            fillPersonalInfo(userID);
-            fillFinancialInfo(userID);
+            var fName = faker.name.firstName().replace("'","");
+            var lName = faker.name.lastName().replace("'","");
+            fillPersonalInfo(
+                userID, fName,
+                lName, faker.date.past(55,"1/1/2000").toLocaleDateString("en-US"),
+                faker.internet.email(fName,lName)
+            );
+            fillFinancialInfo(
+                userID, faker.finance.routingNumber(),
+                faker.finance.account(), faker.finance.amount()
+            );
         }
     });
 }
-function fillPersonalInfo(userID){
-    var fName = faker.name.firstName().replace("'","");
-    var lName = faker.name.lastName().replace("'","");
-    var query = `INSERT INTO personal_info VALUES (DEFAULT,'${fName}','${lName}','${faker.date.past(55,"1/1/2000").toLocaleDateString("en-US")}','${faker.internet.email(fName,lName)}',${userID})`
-    dbCon.runDBQuery(query)
-}
-function fillFinancialInfo(userID){
-    var query = `INSERT INTO financial_info VALUES (DEFAULT,${userID},${faker.finance.routingNumber()},${faker.finance.routingNumber()},${faker.finance.amount()})`
-    dbCon.runDBQuery(query)
-}
 // To init the tables, we can add to a like /resettables(x) route
-// fillTables(5)
+fillTables(5)
