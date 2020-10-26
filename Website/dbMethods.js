adb72a9d418f548fb2bf93654125cdfea9d47d30
nstapler <staplern@bu.edu>
2020-10-25 21:19:43 -0700

    Show user balance/ accountid

--- Website/dbMethods.js
+++ Website/dbMethods.js
@@ -4,4 +4,8 @@ function deposit(amount, userid, callback){
     var query = `UPDATE financial_info SET balance = balance + ${amount} WHERE user_id=${userid}`
     dbCon.runDBQuery(query,callback);
 }
-module.exports = {deposit}
+function getDepositInfo(userid, callback){
+    var query = `SELECT balance, account_number FROM financial_info WHERE user_id=${userid}`
+    dbCon.runDBQuery(query,callback);
+}
+module.exports = {deposit,getDepositInfo}
