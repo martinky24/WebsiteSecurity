f43ad656b783da3bb667e9e22adbf28804512eb9
Kyle Martin <kmartin@wolfram.com>
2020-11-05 22:49:46 -0700

    implementation for broken authentication

--- Website/routes/admin.js
+++ Website/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 let queries = require("../data/queries");
 
 router.get('/admin', async function(req, res, next) {
@@ -12,6 +13,25 @@ router.get('/admin', async function(req, res, next) {
     const result = await queries.getAllFinancialInfo();
     content.data = result.rows;
     res.render('pages/admin', content);
+});
+
+router.get('/logs', async function(req, res, next) {
+    if ((req.session.secure) && (req.session.uname != "admin")) {
+        return res.redirect('/home');
+    }
+    res.sendFile('logs/access.log', { root: '.' })
+});
+
+router.post('/resetlogs', async function(req, res, next) {
+    if ((req.session.secure) && (req.session.uname != "admin")) {
+        return res.redirect('/home');
+    }
+    var options = { flag : 'w' };
+    fs.writeFile('logs/access.log', "", options, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
+    res.redirect("/admin");
 });
 
 module.exports = router;
--- Website/routes/login.js
+++ Website/routes/login.js
@@ -13,6 +13,27 @@ router.get('/login', function(req, res, next) {
 		secure: req.session.secure,
 	}, req.savedContext));
 });
+
+router.get('/loginUserInsecure', async function(req, res, next) {
+	var password = req.query.pword;
+	var username = req.query.loginUsername
+	
+	// check if username exists and get hashedpass
+	const result = await queries.checkValidUsername(username);
+
+	// check if password matches hashed
+	if (result.rows.length > 0 && bcrypt.compareSync(password, result.rows[0].password_hash)) {
+		req.session.uname = username;
+		req.session.userID = result.rows[0].user_id // Store for easy personal/financial lookup
+		console.log('Insecurely logging in as user ',req.session.uname, "\nWith uid:", req.session.userID);
+		await rMethods.saveSession(req);
+		res.redirect("/home");
+	} else {
+		await rMethods.saveSessionContext({error:"Incorrect Username or Password"},req);
+        return res.redirect(req.headers.referer);
+	}
+});
+
 router.post('/loginUser', async function(req, res, next) {
 	var password = req.body.pword;
 	var username = req.body.loginUsername
@@ -24,7 +45,7 @@ router.post('/loginUser', async function(req, res, next) {
 	if (result.rows.length > 0 && bcrypt.compareSync(password, result.rows[0].password_hash)) {
 		req.session.uname = username;
 		req.session.userID = result.rows[0].user_id // Store for easy personal/financial lookup
-		console.log('Logging in as user ',req.session.uname, "\nWith uid:", req.session.userID);
+		console.log('Securely logging in as user ',req.session.uname, "\nWith uid:", req.session.userID);
 		await rMethods.saveSession(req);
 		res.redirect("/home");
 	} else {
